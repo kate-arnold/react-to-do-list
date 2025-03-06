d274f20bc829db3b1bb6032c8be2a23ab3657bd0
Kate Arnold <55087064+kate-arnold@users.noreply.github.com>
2025-03-06 23:42:52 +0000

    Continue with basic functionality and enhance styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import "./styles.css";
 import React from "react";
-import Form from "/components/Form";
-import Header from "/components/Header";
-import TODOHero from "/components/TODOHero";
-import TODOList from "/components/TODOList";
+import {useState} from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import TODOHero from "./components/TODOHero";
+import TODOList from "./components/TODOList";
 
 function Home() {
+  const [todos, setTodos] = useState([
+    {title: 'Task A', id: self.crypto.randomUUID(), is_completed: false},
+    {title: 'Task B', id: self.crypto.randomUUID(), is_completed: true},
+    {title: 'Task C', id: self.crypto.randomUUID(), is_completed: false},
+  ]);
+
   return (
     <div className="wrapper">
       <Header />
       <TODOHero todos_completed={0} total_todos={0} />
       <Form />
-      <TODOList todos={[]} />
+      <TODOList todos={todos} />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <header>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+        <rect width="24" height="24" rx="8" ry="8" fill="#e3dacb" />
+        <circle cx="12" cy="12" r="4" />
+      </svg>
+      <h1>TODO</h1>
+    </header>
+  );
+}
+
+export default Header;
