37ffa5f26c8821afc3dc58749b95b8868eecc8e0
kate-arnold <kate@kateandbenjy.com>
2025-03-14 16:06:58 +0000

    Add functionality to mark todos as complete;  start adding edit function

--- src/components/TODOList.jsx
+++ src/components/TODOList.jsx
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+import {useState} from "react";
+import {useRef} from "react";
+
 function Item({ item, setTodos }) {
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef(null);
+
   const completeToDo = () => {
     setTodos((prevTodos) =>
       prevTodos.map((todo) =>
@@ -9,28 +16,69 @@ function Item({ item, setTodos }) {
     );
   }
 
+  const handleEdit = () => {
+    setEditing(true);
+  }
+
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.setSelectionRange(
+        inputRef.current.value.length,
+        inputRef.current.value.length
+      );
+    }
+  }, [editing]);
+
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+    setEditing(false);
+  };
+
+  const handleInputBlur = () => {
+    setEditing(false);
+  };
+
   return (
     <li id={item?.id} className="todo_item">
-      <button className="todo_items_left" onClick={completeToDo}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="currentColor" fill={item.is_completed ? "currentColor" : "none"}>
-          <circle cx="11" cy="11" r="8" />
-        </svg>
-        <p style={item.is_completed ? { textDecoration: "line-through" } : {}}>{item?.title}</p>
-      </button>
-      <div className="todo_items_right">
-        <button>
-          <span className="visually-hidden">Edit</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="currentColor" strokeWidth="2" fill="none"> 
-            <path d="M20,8 L8,20 L4,20 L4,16 L16,4z M3,23 L20,23" />
-          </svg>
-        </button>
-        <button>
-          <span className="visually-hidden">Delete</span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="red" strokeWidth="2" fill="none"> 
-            <path d="M4,4 L20,20 M4,20 L20,4" />
-          </svg>
-        </button>
-      </div>
+      {editing ? (
+        <form className="edit-form" onSubmit={handleInpuSubmit}>
+          <label htmlFor="edit-todo">
+            <input
+              ref={inputRef}
+              type="text"
+              name="edit-todo"
+              id="edit-todo"
+              defaultValue={item?.title}
+              onBlur={handleInputBlur}
+              onChange={handleInputChange}
+            />
+          </label>
+        </form>
+      ) : (
+        <>
+          <button className="todo_items_left" onClick={completeToDo}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="currentColor" fill={item.is_completed ? "currentColor" : "none"}>
+              <circle cx="11" cy="11" r="8" />
+            </svg>
+            <p style={item.is_completed ? { textDecoration: "line-through" } : {}}>{item?.title}</p>
+          </button>
+          <div className="todo_items_right">
+            <button>
+              <span className="visually-hidden">Edit</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="currentColor" strokeWidth="2" fill="none"> 
+                <path d="M20,8 L8,20 L4,20 L4,16 L16,4z M3,23 L20,23" />
+              </svg>
+            </button>
+            <button>
+              <span className="visually-hidden">Delete</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" stroke="red" strokeWidth="2" fill="none"> 
+                <path d="M4,4 L20,20 M4,20 L20,4" />
+              </svg>
+            </button>
+          </div>
+        </>
+      )}
     </li>
   );
 }
